5baa2fcf9ba00100517b6f76325e3fa6661005f1
FYYKX <lvkangxi@gmail.com>
2017-12-06 22:06:01 +0800

    Disable pagination for btc

--- public/javascripts/btc.js
+++ public/javascripts/btc.js
@@ -1,8 +1,9 @@
-$(function () {
+$(function() {
   var table = $("table").DataTable({
+    "paging": false,
     "ajax": {
       "url": "btc.json",
-      "dataSrc": function (json) {
+      "dataSrc": function(json) {
         var ask = json.ask;
         var ticker = json.ticker;
         for (var i = 0, ien = ticker.length; i < ien; i++) {
@@ -16,8 +17,7 @@ $(function () {
     "order": [
       [4, "desc"]
     ],
-    "columns": [
-      {
+    "columns": [{
         "data": "exchange"
       },
       {
@@ -36,14 +36,14 @@ $(function () {
     "columnDefs": [{
       "targets": 4,
       "data": "percentage",
-      "render": function (data, type, row, meta) {
+      "render": function(data, type, row, meta) {
         var css = data > 0 ? "badge-success" : "badge-danger";
         return "<span class='badge " + css + "'>" + (data * 100).toFixed(2) + "%" + "</span>";
       }
     }]
   });
 
-  setInterval(function () {
+  setInterval(function() {
     table.ajax.reload();
   }, 10 * 1000);
 });
