d30fd9daed397e3691816b2fde5d605a6e3d352f
FYYKX <lvkangxi@gmail.com>
2018-01-04 15:48:18 +0800

    Remove config

--- public/javascripts/qe.js
+++ public/javascripts/qe.js
@@ -1,75 +1,73 @@
 $(function () {
   if ("Notification" in window) {
-      Notification.requestPermission().then(function (result) {
-          console.log(result);
-      });
+    Notification.requestPermission().then(function (result) {
+      console.log(result);
+    });
   }
 
   var last = 0;
   var table = $("#qe").DataTable({
-      "ajax": {
-          "url": "qqb.json",
-          "data": function (d) {
-              d.currency = "ETH";
-          },
-          "dataSrc": function (json) {
-              var chance = json.chance;
-              if (chance > 0.01 && chance > last) {
-                  if ($("#" + json.sell + "_sell").val() == "YES" && $("#" + json.buy + "_buy").val == "YES") {
-                      new Notification("QASHETH " + (chance * 100).toFixed(2) + "%", {
-                          body: "Sell at " + json.sell + " Buy at " + json.buy,
-                          icon: "/images/qash.png"
-                      });
-                  }
-                  last = chance;
-              }
-              return json.ticker;
-          }
+    "ajax": {
+      "url": "qqb.json",
+      "data": function (d) {
+        d.currency = "ETH";
       },
-      "ordering": false,
-      "columns": [
-          {
-              "data": "exchange"
-          },
-          {
-              "data": "ask"
-          },
-          {
-              "data": "bid"
-          },
-          {
-              "data": "quoine"
-          },
-          {
-              "data": "qryptos"
-          },
-          {
-              "data": "bitfinex"
+      "dataSrc": function (json) {
+        var chance = json.chance;
+        if (chance > 0.01 && chance > last) {
+          new Notification("QASHETH " + (chance * 100).toFixed(2) + "%", {
+            body: "Sell at " + json.sell + " Buy at " + json.buy,
+            icon: "/images/qash.png"
+          });
+          last = chance;
+        }
+        return json.ticker;
+      }
+    },
+    "ordering": false,
+    "columns": [
+      {
+        "data": "exchange"
+      },
+      {
+        "data": "ask"
+      },
+      {
+        "data": "bid"
+      },
+      {
+        "data": "quoine"
+      },
+      {
+        "data": "qryptos"
+      },
+      {
+        "data": "bitfinex"
+      }
+    ],
+    "columnDefs": [
+      {
+        "targets": 0,
+        "data": "exchange",
+        "render": function (data, type, row, meta) {
+          return "<span class='label " + data + "'>" + data + "</span>";
+        }
+      },
+      {
+        "targets": [3, 4, 5],
+        "render": function (data, type, row, meta) {
+          if (data) {
+            var css = data > 0 ? "label-success" : "label-danger";
+            return "<span class='label " + css + "'>" + (data * 100).toFixed(2) + "%" + "</span>";
+          } else {
+            return "";
           }
-      ],
-      "columnDefs": [
-          {
-              "targets": 0,
-              "data": "exchange",
-              "render": function (data, type, row, meta) {
-                  return "<span class='label " + data + "'>" + data + "</span>";
-              }
-          },
-          {
-              "targets": [3, 4, 5],
-              "render": function (data, type, row, meta) {
-                  if (data) {
-                      var css = data > 0 ? "label-success" : "label-danger";
-                      return "<span class='label " + css + "'>" + (data * 100).toFixed(2) + "%" + "</span>";
-                  } else {
-                      return "";
-                  }
-              }
-          }]
+        }
+      }]
   });
 
   //API users should not make more than 300 requests per 5 minute
   setInterval(function () {
-      table.ajax.reload();
+    table.ajax.reload();
   }, 20 * 1000);
 });
